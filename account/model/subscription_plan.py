a2d9227652e338e946f274a5f21e6eb245641ff9
mcharolabs <mcharoprofg23@gmail.com>
2025-01-18 00:58:53 +0300

    subscription label

--- account/model/subscription_plan.py
+++ account/model/subscription_plan.py
@@ -8,6 +8,9 @@ class SubscriptionPlan(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     max_houses = models.IntegerField()
     duration_days = models.IntegerField(default=30)
+    
+    class Meta:
+        db_table = 'subscription_plan'
 
     def __str__(self):
         return f"{self.name} - ${self.price} (Max {self.max_houses} Houses)"
