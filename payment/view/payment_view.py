7653aeb78823b2a3d3199ffcc4c0ecad0d9b98a6
mcharolabs <mcharoprofg23@gmail.com>
2025-01-24 18:46:21 +0300

    debug

--- payment/view/payment_view.py
+++ payment/view/payment_view.py
@@ -16,15 +16,15 @@ class PaymentWebHook(APIView):
     
     @action(detail=False, methods=['post'])
     @csrf_exempt
-    def post(self, request: HttpRequest, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         
         if request.data:
-            
-            data = request.data.decode('utf-8')
-            logger.info(f"Webhook received data: {data}")
+            data = request.data
+            # data = request.data.decode('utf-8')
             
             try:
-                Payment.on_complete_payment(payment_status=data.payment_status, reference=data.reference)
+                logger.info(f"Webhook received data: {data}")
+                # Payment.on_complete_payment(payment_status=data.payment_status, reference=data.reference)
             except:
                 return HttpResponse('An error occurred processing the webhook data.', status=400)
         return HttpResponse('Empty request body.', status=400)
