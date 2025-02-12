1e73bd79a182d5efc039f638c1901371d24628fb
mcharolabs <mcharoprofg23@gmail.com>
2025-02-12 14:07:51 +0300

    changes admin message queue

--- message/admin.py
+++ message/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import MessageQueue
 
-@admin.register(MessageQueue)
 class MessageQueueAdmin(admin.ModelAdmin):
-    list_display = ('message_id', 'to', 'description', 'name', 'group_name')
-    search_fields = ('message_id', 'to', 'name', 'group_name')
-    list_filter = ('group_name', 'payment')
-    ordering = ('-id',)
+    list_display = ("to", "description", "name", "group_name", "actions")
+    search_fields = ("to", "name", "group_name", "description")
+    list_filter = ("group_name",)
+    ordering = ("-id",)
+
+    fieldsets = (
+        ("Message Details", {
+            "fields": ("message", "to", "description", "name", "group_name", "payment")
+        }),
+    )
+
+    def actions(self, obj):
+        """Add a clickable button to view payment details."""
+        if obj.payment:
+            return format_html(
+                '<a href="/admin/app_name/payment/{}/change/" class="button" '
+                'style="padding:5px 10px; background:#3498db; color:#fff; border-radius:5px;">View Payment</a>',
+                obj.payment.payment_id 
+            )
+        return format_html('<span style="color: red;">No Payment</span>')
+
+    actions.short_description = "Actions"
+
+admin.site.register(MessageQueue, MessageQueueAdmin)
