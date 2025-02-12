2f94463a9396d677583c0dfe90af9a83230a26ca
mcharolabs <mcharoprofg23@gmail.com>
2025-02-12 13:56:56 +0300

    removed payment id

--- message/admin.py
+++ message/admin.py
@@ -3,7 +3,7 @@ from .models import MessageQueue
 
 @admin.register(MessageQueue)
 class MessageQueueAdmin(admin.ModelAdmin):
-    list_display = ('message_id', 'payment', 'to', 'description', 'name', 'group_name')
+    list_display = ('message_id', 'to', 'description', 'name', 'group_name')
     search_fields = ('message_id', 'to', 'name', 'group_name')
     list_filter = ('group_name', 'payment')
     ordering = ('-id',)
