8abf14ba480640af835e0e887a4e423b6d9bd367
mcharolabs <mcharoprofg23@gmail.com>
2025-02-12 14:18:54 +0300

    changes admin message queue

--- message/admin.py
+++ message/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.urls import reverse
 from .models import MessageQueue
 
 class MessageQueueAdmin(admin.ModelAdmin):
-    list_display = ("to", "description", "name", "group_name", "view_payment_action")
+    list_display = ("to", "description", "name", "group_name", "view_details")
     search_fields = ("to", "name", "group_name", "description")
     list_filter = ("group_name",)
     ordering = ("-id",)
@@ -14,16 +15,17 @@ class MessageQueueAdmin(admin.ModelAdmin):
         }),
     )
 
-    def view_payment_action(self, obj):
-        """Add a clickable button to view payment details."""
-        if obj.payment:
-            return format_html(
-                '<a href="/admin/app_name/payment/{}/change/" class="button" '
-                'style="padding:5px 10px; background:#3498db; color:#fff; border-radius:5px;">View Payment</a>',
-                obj.payment.payment_id 
-            )
-        return format_html('<span style="color: red;">No Payment</span>')
+    def view_details(self, obj):
+        """Add a clickable button to view message queue details."""
+        change_url = reverse(
+            "admin:{}_{}_change".format(obj._meta.app_label, obj._meta.model_name),
+            args=[obj.pk]
+        )
+        return format_html(
+            '<a href="{}" class="button" style="padding:5px 10px; background:#3498db; color:#fff; border-radius:5px;">View</a>',
+            change_url
+        )
 
-    view_payment_action.short_description = "Actions"
+    view_details.short_description = "Actions"
 
 admin.site.register(MessageQueue, MessageQueueAdmin)
