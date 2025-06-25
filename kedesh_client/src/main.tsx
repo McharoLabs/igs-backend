463c5186714cde5c901d846bcb167ba936d218d3
mcharolabs <mcharoprofg23@gmail.com>
2025-06-25 04:50:24 +0300

    renamed file

--- kedesh_client/src/main.tsx
+++ kedesh_client/src/main.tsx
@@ -16,7 +16,7 @@ Array.prototype.map = function <U>(
 // Now normal imports
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./app.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
