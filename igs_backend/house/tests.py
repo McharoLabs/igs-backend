a422adfed4a0a738a9554eb0566797b22dbb3eb3
mcharolabs <mcharoprofg23@gmail.com>
2025-07-03 21:55:02 +0300

    django test file

--- igs_backend/house/tests.py
+++ igs_backend/house/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_basic_addition(self):
+        """
+        Test that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+    def test_string_concatenation(self):
+        """
+        Test that concatenating 'Hello' and ' World' results in 'Hello World'.
+        """
+        self.assertEqual('Hello' + ' World', 'Hello World')
+
+    def test_list_length(self):
+        """
+        Test that the length of the list [1, 2, 3] is 3.
+        """
+        self.assertEqual(len([1, 2, 3]), 3)
