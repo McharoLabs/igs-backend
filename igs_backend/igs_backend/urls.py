43f8f544760540b708d30033889c072e3a6e081e
mcharolabs <mcharoprofg23@gmail.com>
2025-07-15 15:31:33 +0300

    token fix

--- igs_backend/igs_backend/urls.py
+++ igs_backend/igs_backend/urls.py
@@ -22,9 +22,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("admin/", admin.site.urls),
     path('api/v2/payment/', include('payment.urls')),
     path('api/v2/user/', include('user.urls')),
